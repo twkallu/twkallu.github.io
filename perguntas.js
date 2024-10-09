9970aedf82399cb03f00de69dc848a0e6bf79ae5
twkallu <159554713+twkallu@users.noreply.github.com>
2024-10-09 01:09:06 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,60 @@
+criaCartao(
+    'geografia',
+    'qual o menor país do mundo?',
+    'o Vaticano'
+)
+
+criaCartao(
+    'matematica',
+    'quantas casas decimais tem o número pi?',
+    'tem infinitas casas decimais'
+)
+
+criaCartao(
+    'história',
+    'quem pintou mona lisa?',
+    'leonardo da vinci'
+)
+
+criaCartao(
+    'quimica',
+    'qual o nome popular do cloreto de sódio?',
+    'sal de cozinha'
+)
+
+criaCartao(
+    'ciencias',
+    'qual é o único mamífero que voa?',
+    'o morcego'
+)
+
+criaCartao(
+    'história 2.0',
+    'em que ano o homem pisou na Lua?',
+    'em 1969'
+)
+
+criaCartao(
+    'literatura',
+    'qual é a série de livros mais vendida do século 21?',
+    'harry potter'
+)
+
+criaCartao(
+    'música',
+    'quantas teclas há em um piano clássico?',
+    '88 teclas'
+)
+
+criaCartao(
+    'teologia',
+    'quem era o antigo Deus Grego do Sol? ',
+    'Apolo'
+)
+
+criaCartao(
+    'química 2.0',
+    'quantos elementos existem na tabela periódica?',
+    '118 elementos'
+)
+
